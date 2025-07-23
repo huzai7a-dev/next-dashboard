016d4f4ac3fc301c5f556f39bf2d0cb34d5e0996
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2025-07-23 23:00:56 +0500

    lint fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,8 +60,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // If a database error occurs, return a more specific error.
-    return {
+    console.error('Database Error:', error);
+    return {  
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
